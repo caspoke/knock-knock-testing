8f0b82b217529bc54ffcd23adf0e530872980bfa
lucas <lucas@LAPTOP-DVL2QTRU.attlocal.net>
2021-02-18 16:46:30 -0500

    mudança de nomes nos arquivos/pastas

--- KnockKnockTesting/src/DataCollector.java
+++ KnockKnockTesting/src/DataCollector.java
@@ -1,8 +1,8 @@
-package test;
+
 import collectingmodule.ContextCollector;
 import core.communication.ArduinoListener;
 
-public class CollectingTest {
+public class DataCollector {
 	public static void main (String args[]) {
 		ContextCollector c = new ContextCollector("context");
 		c.setPort("COM4");
